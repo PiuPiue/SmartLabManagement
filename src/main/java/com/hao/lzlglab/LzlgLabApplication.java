15404fc2d30e273f56949e8dc47d220fe4493d62
POJO <2917967684@qq.com>
2024-07-16 09:45:56 +0800

    first init

---
+++ src/main/java/com/hao/lzlglab/LzlgLabApplication.java
@@ -0,0 +1,13 @@
+package com.hao.lzlglab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LzlgLabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LzlgLabApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hao/lzlglab/LzlgLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hao.lzlglab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LzlgLabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
